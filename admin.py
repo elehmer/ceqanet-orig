cbb822e655b78e4c1620a481d31bfea39091dcfb
Alex Mandel <aimandel@ucdavis.edu>
2013-09-21 23:07:33 -0700

    Implement geodjango admin, center on California, need to override tools for a GeometryCollection

--- admin.py
+++ admin.py
@@ -1,4 +1,11 @@
-#from ceqanet.models import DocTypes
-#from django.contrib import admin
+from django.contrib.gis import admin
+from ceqanet.models import documents, Locations
 
-#admin.site.register(DocTypes)
+class CaliforniaAdmin(admin.OSMGeoAdmin):
+    default_lon= -13449176
+    default_lat= 4546224    
+    default_zoom= 6
+
+
+admin.site.register(documents)
+admin.site.register(Locations, CaliforniaAdmin)
